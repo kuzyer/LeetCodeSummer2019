027a26264ae5ab43383394a1d33d00a9b08ee89c
kuzyer <woyao068@gmail.com>
2019-11-07 15:42:01 +0800

    二分法-LeetCode278.第一个错误的版本

---
+++ test01/src/test007.java
@@ -0,0 +1,22 @@
+//二分法-LeetCode278.第一个错误的版本
+public class test007 {
+    public static int[] a = {0,1,1,1,2,2};
+
+    public static int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+//            if (isBadVersion(mid)) {
+            if (a[mid] == 2) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstBadVersion(5));
+    }
+}
