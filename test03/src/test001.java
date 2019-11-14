70ea9af895245932a0f0e8b895b883b16edbfe8c
kuzyer <woyao068@gmail.com>
2019-11-14 10:24:20 +0800

    树专题-LeetCode98.验证二叉搜索树

--- test03/src/test001.java
+++ test03/src/test001.java
@@ -1,15 +1,36 @@
+import java.util.Stack;
+
 //树专题-LeetCode98.验证二叉搜索树
 public class test001 {
-    //DFS
+//    //递归 DFS
+//    public boolean isValidBST(TreeNode root) {
+//        return isValidTree(root, null, null);
+//    }
+//
+//    public boolean isValidTree(TreeNode root, Integer min, Integer max) {
+//        if (root == null) return true;
+//        if (min != null && root.val <= min) return false;
+//        if (max != null && root.val >= max) return false;
+//
+//        return isValidTree(root.left, min, root.val) && isValidTree(root.right, root.val, max);
+//    }
+
+    //迭代 BFS
     public boolean isValidBST(TreeNode root) {
-        return isValidTree(root, null, null);
-    }
-
-    public boolean isValidTree(TreeNode root, Integer min, Integer max) {
         if (root == null) return true;
-        if (min != null && root.val <= min) return false;
-        if (max != null && root.val >= max) return false;
-
-        return isValidTree(root.left, min, root.val) && isValidTree(root.right, root.val, max);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
     }
+
 }
