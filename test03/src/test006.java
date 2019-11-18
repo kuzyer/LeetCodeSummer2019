9640cf3b6068bcd1ae6090d1327ac7b701537c62
kuzyer <woyao068@gmail.com>
2019-11-18 11:13:03 +0800

    树专题-LeetCode236.二叉树的最近公共祖先

---
+++ test03/src/test006.java
@@ -0,0 +1,19 @@
+//树专题-LeetCode236.二叉树的最近公共祖先
+public class test006 {
+    //递归
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root.val == p.val || root.val == q.val) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        }
+        return null;
+    }
+}
