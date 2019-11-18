a2d4fdf7da8793356e16935605933f18f65497b4
kuzyer <woyao068@gmail.com>
2019-11-18 21:47:49 +0800

    树专题-LeetCode543.二叉树的直径

---
+++ test03/src/test008.java
@@ -0,0 +1,17 @@
+//树专题-LeetCode543.二叉树的直径
+public class test008 {
+    public int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null) return 0;
+        int d1 = depth(root.left);
+        int d2 = depth(root.right);
+        ans = Math.max(ans, d1 + d2 + 1);
+        return Math.max(d1, d2) + 1;
+    }
+}
