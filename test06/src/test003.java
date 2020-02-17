3af905f0e9a8696d5316f0eca96ce46a0b488e47
kuzyer <woyao068@gmail.com>
2020-02-17 11:57:39 +0800

    滑动窗口、双指针与单调队列/栈专题-LeetCode26.删除排序数组中的重复项

---
+++ test06/src/test003.java
@@ -0,0 +1,14 @@
+//滑动窗口、双指针与单调队列/栈专题-LeetCode26.删除排序数组中的重复项
+public class test003 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
