3466039a1a4865f4c34f4725b22f9863118cf789
kuzyer <woyao068@gmail.com>
2020-02-24 10:53:13 +0800

    基本数据结构专题-LeetCode547.朋友圈

--- test07/src/test006.java
+++ test07/src/test006.java
@@ -29,4 +29,52 @@ public class test006 {
 
         return res;
     }
+
+    //定义了模板
+
+//    public class UnionFind {
+//        private int[] father;
+//        private int count;
+//        public UnionFind (int n) {
+//            father = new int[n];
+//            for (int i = 0; i < n; i++) {
+//                father[i] = i;
+//            }
+//        }
+//        int find(int x) {
+//            if (father[x] == x) {
+//                return x;
+//            }
+//            //路径压缩
+//            father[x] = find(father[x]);
+//            return father[x];
+//        }
+//        void union(int a, int b){
+//            int fa = find(a);
+//            int fb = find(b);
+//            father[fb] = fa;
+//        }
+//    }
+//    class Solution {
+//
+//        public int findCircleNum(int[][] M) {
+//            int n = M.length;
+//            UnionFind u = new UnionFind(n);
+//            for (int i = 0; i < n; i++) {
+//                for (int j = 0; j < i; j++) {
+//                    if (M[i][j] == 1)
+//                        u.union(i, j); //两个人是朋友则合并朋友圈
+//                }
+//            }
+//
+//            int res = 0;
+//            for (int i = 0; i < n; i++) {
+//                if (u.find(i) == i) {
+//                    res++; //计算朋友圈的个数
+//                }
+//            }
+//
+//            return res;
+//        }
+//    }
 }
