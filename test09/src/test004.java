a3957eee73023d174f2ef094c44abce5c5c9ecd2
kuzyer <woyao068@gmail.com>
2020-03-12 12:42:21 +0800

    排序算法模板-希尔排序

---
+++ test09/src/test004.java
@@ -0,0 +1,37 @@
+/*
+    排序算法模板-希尔排序
+
+    平均时间复杂度O(n1.3)
+    最好情况O(n)
+    最坏情况O(n2)
+    空间复杂度O(1)
+    不稳定
+ */
+public class test004 {
+    public static int[] array = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
+        int gap = arr.length / 2;
+        for (; gap > 0; gap = gap / 2) {
+
+            for (int i = gap + 1; i < arr.length; i++) {
+                int j = i - gap;
+                while (j >= 0 && arr[j] > arr[j + gap]) {
+                    arr[j + gap] = arr[j] + arr[j + gap];
+                    arr[j] = arr[j + gap] - arr[j];
+                    arr[j + gap] = arr[j + gap] - arr[j];
+                    j = j - gap;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
