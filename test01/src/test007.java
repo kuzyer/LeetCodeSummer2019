945e24d917c866e7286c7258124ef6b679109022
kuzyer <woyao068@gmail.com>
2020-03-28 16:40:49 +0800

    二分法-LeetCode278.第一个错误的版本

--- test01/src/test007.java
+++ test01/src/test007.java
@@ -16,6 +16,19 @@ public class test007 {
         return l;
     }
 
+//    public int firstBadVersion(int n) {
+//        int l = 1, r = n;
+//        while (l < r) {
+//            int mid = l + (r - l) / 2;
+//            if (isBadVersion(mid)) {
+//                r = mid;
+//            } else {
+//                l = mid + 1;
+//            }
+//        }
+//        return l;
+//    }
+
     public static void main(String[] args) {
         System.out.println(firstBadVersion(5));
     }
